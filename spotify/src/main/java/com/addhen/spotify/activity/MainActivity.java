0d566095b22ad647c037308f465e488769290b34
Henry Addo <henry@gogogenki.com>
2015-06-05 11:50:27 +0900

    Build initial app structure

--- spotify/src/main/java/com/addhen/spotify/activity/MainActivity.java
+++ spotify/src/main/java/com/addhen/spotify/activity/MainActivity.java
@@ -5,25 +5,40 @@ import com.addhen.spotify.fragment.ArtistFragment;
 
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements ArtistFragment.OnFragmentInteractionListener {
 
+    private ArtistFragment mArtistFragment;
+
+    private SearchView mSearchView;
+
+    private String mArtistName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        addFragment(R.id.add_fragment_container, ArtistFragment.newInstance());
+        mArtistFragment = ArtistFragment.newInstance();
+        addFragment(R.id.add_fragment_container, mArtistFragment);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        mSearchView =
+                (SearchView) menu.findItem(R.id.menu_add_search).getActionView();
+        initSearchView();
         return true;
     }
 
@@ -41,6 +56,64 @@ public class MainActivity extends AppCompatActivity
                 .beginTransaction();
         fragmentTransaction.add(containerViewId, fragment);
         fragmentTransaction.commit();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        handleSearchIntent(intent);
+    }
+
+    private void initSearchView() {
+        final SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+
+        mSearchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+        mSearchView.clearFocus();
+        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                mSearchView.clearFocus();
+                performArtistSearch(s);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                if (!TextUtils.isEmpty(s)) {
+                    performArtistSearch(s);
+                }
+                return true;
+            }
+        });
+
+        mSearchView.setOnCloseListener(new SearchView.OnCloseListener() {
+            @Override
+            public boolean onClose() {
+                return false;
+            }
+        });
+
+        if (!TextUtils.isEmpty(mArtistName)) {
+            mSearchView.setQuery(mArtistName, false);
+        }
+    }
+
+    private void handleSearchIntent(Intent intent) {
+
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            query = query == null ? "" : query;
+            mArtistName = query;
+            performArtistSearch(query);
+            if (mSearchView != null) {
+                mSearchView.setQuery(query, false);
+            }
+        }
+    }
+
+    private void performArtistSearch(String artistName) {
+        Toast.makeText(this, "Artist " + artistName + " not found", Toast.LENGTH_LONG).show();
     }
 
     @Override
